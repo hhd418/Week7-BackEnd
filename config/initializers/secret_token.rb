91fb34e8638c5e6ed0002c493c9899e92d03e743
hhd418 <hhd418@gmail.com>
2013-10-18 13:53:18 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gamestarter::Application.config.secret_key_base = '69647687ea7506a92de271bae486e1fe880660f3bbaa17b0dd3d8b675f9cb20412965ad95d68f7bf7867c0d86e5a5775855e154e6300fea764a5e6d33fe21366'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gamestarter::Application.config.session_store :cookie_store, key: '_gamestarter_session'
